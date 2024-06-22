7f746ff85d605ecb08aeef503068ffc045c4b0a6
Hamdan-Vohra <hamdanvohra5676@gmail.com>
2024-06-22 14:41:07 +0500

    Style NavBar and Home

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,44 +1,44 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function Navbar() {
-
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg sticky-top">
+      <div className="container-fluid" >
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/" activeClassName="active">Home</NavLink>
+            </li>
             <li className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <NavLink className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false" activeClassName="active">
                 About
-              </Link>
+              </NavLink>
               <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="/">Developers</Link></li>
-                <li><Link className="dropdown-item" to="/">organization</Link></li>
+                <li><NavLink className="dropdown-item" to="/">Developers</NavLink></li>
+                <li><NavLink className="dropdown-item" to="/">Organization</NavLink></li>
                 <li><hr className="dropdown-divider" /></li>
               </ul>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/adhd">ADHD</Link>
+              <NavLink className="nav-link" to="/adhd" activeClassName="active">ADHD</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/autism">Autism</Link>
+              <NavLink className="nav-link" to="/autism" activeClassName="active">Autism</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/dyslexia">Dyslexia</Link>
+              <NavLink className="nav-link" to="/dyslexia" activeClassName="active">Dyslexia</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/downsyndrome">Downsyndrome</Link>
+              <NavLink className="nav-link" to="/downsyndrome" activeClassName="active">Downsyndrome</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/Cerebral-palsy">Cerebral Palsy</Link>
+              <NavLink className="nav-link" to="/Cerebral-palsy">Cerebral Palsy</NavLink>
             </li>
-
           </ul>
-
         </div>
       </div>
     </nav>
